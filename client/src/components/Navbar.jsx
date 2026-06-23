bfbe7c0d321f94d4ab9bfd0d7304cc7136086f94
alwaysmihirrkadam <mihirkadam1232@gmail.com>
2026-06-23 16:03:13 +0530

    Update Frontend UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = ({ token, user_name }) => {
     toast.success("Logged Out Successfully");
 
     setTimeout(() => {
-      navigate("/api/auth/login");
+      navigate("/login");
       window.location.reload();
     }, 1000);
   };
